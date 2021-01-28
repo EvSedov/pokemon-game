a33286654618d86fca9b254968ba0a51dde7505d
Evgeniy Sedov <sedoff-online@yandex.ru>
2021-01-29 01:50:44 +0700

    Homework 2

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header class="root">
+      <div class="forest"></div>
+      <div class="container">
+        <h1>This is title</h1>
+        <p>This is Description!</p>
+      </div>
+    </header>
+  )
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
-import HeaderBlock from './components/HeaderBlock';
-
-const AppHeader = () => (
-  <h1 className="header">Hello world!!!</h1>
-);
-
-const AppList = () => {
-  const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4']
-  return (
-    <ul>
-      {
-        items.map(item => <li>{item}</li>)
-      }
-    </ul>
-  )
-};
-
-const App = () => {
-  return (
-    <>
-      <AppHeader />
-      <AppList />
-      <HeaderBlock />
-    </>
-  )
-};
 
 ReactDOM.render(<App />, document.getElementById('root'));
