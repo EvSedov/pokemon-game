81b9a2f2880192e0ba7217dc41b98e1af79481a9
Evgeniy Sedov <sedoff-online@yandex.ru>
2021-02-04 23:52:48 +0700

    task 2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,6 +8,8 @@ const Header = ({ title, descr, onClickButton }) => {
   return (
     <header className={s.root}>
       <div className={s.forest}></div>
+      <div className={s.silhouette}></div>
+      <div className={s.moon}></div>
       <div className={s.container}>
         {title && (<h1>{title}</h1>) }
         {descr && (<p>{descr}</p>) }
