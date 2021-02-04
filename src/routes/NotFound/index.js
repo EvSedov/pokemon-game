893ed6e37e7e899144459a4499746e11931ed914
Evgeniy Sedov <sedoff-online@yandex.ru>
2021-02-04 23:46:25 +0700

    task 1

---
+++ src/routes/NotFound/index.js
@@ -0,0 +1,11 @@
+import s from './style.module.css';
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <h1 className={s.title}>404 Page Not Found.</h1>
+    </>
+  )
+};
+
+export default NotFoundPage;
