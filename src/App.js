20fdfe427c87821ac73592eb85a9c79d86d9be72
Evgeniy Sedov <sedoff-online@yandex.ru>
2021-02-09 01:26:33 +0700

    add useContext

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import { useRouteMatch, Route, Switch, Redirect } from 'react-router-dom';
 
 
@@ -10,36 +11,50 @@ import NotFoundPage from './routes/NotFound';
 import MenuHeader from './components/MenuHeader';
 import Footer from './components/Footer';
 
+import {TextContext} from './context/testContext';
+
 import cn from 'classnames';
 
 import s from './App.module.css';
 
 const App = () => {
   const match = useRouteMatch('/');
+
+  const [theme, setTheme] = useState('light');
+  
+  const hendleChangeTheme = (val) => {
+    setTheme(val);
+  }
+
   return (
-    <Switch>
-      <Route path="/404" component={NotFoundPage} />
-      <Route>
-        <>
-          <MenuHeader bgActive={!match.isExact} />
-          <div className={cn(s.wrap, {
-            [s.isHomePage]: match.isExact
-          })}>
-            <Switch>
-              <Route path="/" exact component={HomePage} />
-              <Route path="/home" component={HomePage} />
-              <Route path="/game" component={GamePage} />
-              <Route path="/about" component={AboutPage} />
-              <Route path="/contact" component={ContactPage} />
-              <Route render={() => (
-                <Redirect to="/404"/>
-              )}/>
-            </Switch>
-          </div>
-          <Footer />
-        </>
-      </Route>
-    </Switch>
+    <TextContext.Provider value={{
+      theme,
+      onChangeTheme: hendleChangeTheme
+    }}>
+      <Switch>
+        <Route path="/404" component={NotFoundPage} />
+        <Route>
+          <>
+            <MenuHeader bgActive={!match.isExact} />
+            <div className={cn(s.wrap, {
+              [s.isHomePage]: match.isExact
+            })}>
+              <Switch>
+                <Route path="/" exact component={HomePage} />
+                <Route path="/home" component={HomePage} />
+                <Route path="/game" component={GamePage} />
+                <Route path="/about" component={AboutPage} />
+                <Route path="/contact" component={ContactPage} />
+                <Route render={() => (
+                  <Redirect to="/404"/>
+                )}/>
+              </Switch>
+            </div>
+            <Footer />
+          </>
+        </Route>
+      </Switch>
+    </TextContext.Provider>
   )
 }
 
