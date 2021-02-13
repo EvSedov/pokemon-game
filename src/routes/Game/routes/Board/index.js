c58f14a98ae54dd62bafe049561f59c70722ca25
Evgeniy Sedov <sedoff-online@yandex.ru>
2021-02-14 00:03:03 +0700

    create game play

--- src/routes/Game/routes/Board/index.js
+++ src/routes/Game/routes/Board/index.js
@@ -4,16 +4,23 @@ import { useState, useContext, useEffect } from 'react';
 import {PokemonContext} from '../../../../context/pokemonContext';
 
 import PokemonCard from '../../../../components/PokemonCard';
+import PlayerBoard from './component/PlayerBoard';
 
 import s from './style.module.css';
 
 const BoardPage = () => {
-  const [board, setBoard] = useState([]);
-  const [player2, setPlayer2] = useState([]);
-  console.log("🚀 ~ file: index.js ~ line 13 ~ BoardPage ~ player2", player2)
-  
-  
   const { selectedPokemons } = useContext(PokemonContext);
+
+  const [board, setBoard] = useState([]);
+  const [player1, setPlayer1] = useState(() => {
+    return Object.values(selectedPokemons).map((item) => ({
+      ...item,
+      possession: 'blue'
+    }))
+  });
+  const [player2, setPlayer2] = useState([]);
+  const [choiceCard, setChoiceCard] = useState(null);  
+  
   // const history = useHistory();
 
   useEffect(() => {
@@ -24,7 +31,12 @@ const BoardPage = () => {
 
       const player2Response = await fetch('https://reactmarathon-api.netlify.app/api/create-player');
       const player2Request = await player2Response.json();
-      setPlayer2(player2Request.data);
+      setPlayer2(() => {
+    return player2Request.data.map((item) => ({
+      ...item,
+      possession: 'red'
+    }))
+  });
     };
     fetchData();
   }, []);
@@ -33,28 +45,37 @@ const BoardPage = () => {
   //   history.replace('/game');
   // }
 
-  const hendleClickBoardPlate = (position) => {
-    console.log("🚀 ~ file: index.js ~ line 29 ~ hendleClickBoardPlate ~ position", position)
+  const hendleClickBoardPlate = async (position) => {
+    if (choiceCard) {
+      const params = {
+        position,
+        card: choiceCard,
+        board,
+      }
 
+      const res = await fetch('https://reactmarathon-api.netlify.app/api/players-turn', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(params),
+      });
+
+      const request = await res.json();
+      console.log('#### request', request);
+      setBoard(request.data);
+    }
   }
 
   return (
     <div className={s.root}>
 			<div className={s.playerOne}>
         {
-          Object.values(selectedPokemons).map(
-            ({id, name, type, img, values }) => <PokemonCard
-              key = {id}
-              name = {name}
-              type = {type}
-              img = {img}
-              id = {id}
-              values = {values}
-              isActive
-              minimize
-              className={s.card}
-            />
-          )
+          <PlayerBoard
+            player={1}
+            cards={player1} 
+            onClickCard={(card) => setChoiceCard(card)}
+          />
         }
       </div>
       <div className={s.board}>
@@ -66,28 +87,18 @@ const BoardPage = () => {
               onClick={ () => !item.card && hendleClickBoardPlate(item.position)}
             >
               {
-                item.card && <PokemonCard {...item} minimize />
+                item.card && <PokemonCard {...item.card} isActive minimize />
               }
             </div>
           ))
         }
       </div>
       <div className={s.playerTwo}>
-        {
-          player2.map(
-            ({id, name, type, img, values }) => <PokemonCard
-              key = {id}
-              name = {name}
-              type = {type}
-              img = {img}
-              id = {id}
-              values = {values}
-              isActive
-              minimize
-              className={s.card}
-            />
-          )
-        }
+        <PlayerBoard
+          player={2}
+          cards={player2}
+          onClickCard={(card) => setChoiceCard(card)}
+        />
       </div>
     </div>
   )
